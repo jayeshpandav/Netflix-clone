c7ef396c2537d49ce596417ac7447a75600a1546
Jayesh <jayeshrpandav@gmail.com>
2023-03-07 02:41:12 +0530

    documentries row bug fixed

--- src/Pages/HomeScreen.js
+++ src/Pages/HomeScreen.js
@@ -16,7 +16,7 @@ const HomeScreen = () => {
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
